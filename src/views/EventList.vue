7c023880835cffd5a3491c29c394f5c4ec1fc4ad
Rafael Ramirez <susurrus.ater@gmail.com>
2021-12-19 20:08:00 -0300

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>Lista de eventos</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    EventCard,
+  },
+
+  data() {
+    return {
+      events: [
+        {
+          id: 123456789,
+          category: "cuidado animal",
+          title: "Dia de adopción",
+          description: "Dia de adopción de animales",
+          location: "Rlyeh",
+          date: "Junio 06, 1966",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Hastur",
+        },
+        {
+          id: 123456789,
+          category: "cuidado animal",
+          title: "Dia de adopción",
+          description: "Dia de adopción de animales",
+          location: "Rlyeh",
+          date: "Junio 06, 1966",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Hastur",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
